4e489ec2faf1a5729026181436c0752b4243f723
Ashish Goyal <ashish.19goyal@gmail.com>
2021-11-17 00:39:46 +0530

    Updated test engine to look for load test annotation for selecting tests

--- app/src/main/java/junit/load/test/LoadTestEngine.java
+++ app/src/main/java/junit/load/test/LoadTestEngine.java
@@ -3,9 +3,14 @@ package junit.load.test;
 import org.junit.jupiter.engine.config.CachingJupiterConfiguration;
 import org.junit.jupiter.engine.config.DefaultJupiterConfiguration;
 import org.junit.jupiter.engine.config.JupiterConfiguration;
-import org.junit.jupiter.engine.descriptor.JupiterEngineDescriptor;
-import org.junit.jupiter.engine.discovery.DiscoverySelectorResolver;
+import org.junit.jupiter.engine.descriptor.TestMethodTestDescriptor;
+import org.junit.platform.commons.support.AnnotationSupport;
 import org.junit.platform.engine.*;
+import org.junit.platform.engine.discovery.ClassSelector;
+import org.junit.platform.engine.support.descriptor.AbstractTestDescriptor;
+import org.junit.platform.engine.support.descriptor.ClassSource;
+import org.junit.platform.engine.support.descriptor.EngineDescriptor;
+import java.lang.reflect.Method;
 
 /**
  * This is a custom implementation of test engine.
@@ -13,6 +18,7 @@ import org.junit.platform.engine.*;
  * Only the tests annotated with {@link LoadTest} are executed by this engine
  * The test results report is generated in csv, json and html formats in the build folder
  * Link to html report - build/load-test/index.html
+ *
  * @author Ashish Goyal
  */
 public class LoadTestEngine implements TestEngine {
@@ -26,18 +32,83 @@ public class LoadTestEngine implements TestEngine {
     private LoadTestReporter loadTestReporter = new LoadTestReporter(csvHelper, jsonHelper, htmlHelper);
     private LoadTestExecutor loadTestExecutor = new LoadTestExecutor(loadTestReporter);
 
+    /**
+     * Inherited from TestEngine. Mandatorily override
+     * Defines a uniqueId for the test engine
+     * @return
+     */
     @Override
     public String getId() {
         return "load-test-engine";
     }
 
+    /**
+     * Inherited from TestEngine. Mandatorily override
+     * Find all the test cases to be executed by this Test Engine
+     * @param request
+     * @param uniqueId UniqueId of the Test Engine
+     * @return TestDescriptor that contains all the test cases to be executed
+     */
     @Override
-    public TestDescriptor discover(EngineDiscoveryRequest discoveryRequest, UniqueId uniqueId) {
+    public TestDescriptor discover(EngineDiscoveryRequest request, UniqueId uniqueId) {
+
+        EngineDescriptor engineDescriptor = new EngineDescriptor(uniqueId, "Load Test");
         JupiterConfiguration configuration = new CachingJupiterConfiguration(
-			new DefaultJupiterConfiguration(discoveryRequest.getConfigurationParameters()));
-		JupiterEngineDescriptor engineDescriptor = new JupiterEngineDescriptor(uniqueId, configuration);
-        new DiscoverySelectorResolver().resolveSelectors(discoveryRequest, engineDescriptor);
-		return engineDescriptor;
+                       new DefaultJupiterConfiguration(request.getConfigurationParameters()));
+
+        addTestClassDescriptors(request, engineDescriptor, uniqueId, configuration);
+        return engineDescriptor;
+    }
+
+    /**
+     * Identify all the classes that contains test cases to be executed
+     * @param request
+     * @param engineDescriptor
+     * @param engineId
+     * @param configuration
+     */
+    private void addTestClassDescriptors(EngineDiscoveryRequest request, TestDescriptor engineDescriptor,
+                                         UniqueId engineId, JupiterConfiguration configuration) {
+        request.getSelectorsByType(ClassSelector.class).forEach(selector -> {
+            boolean selectAllMethods = AnnotationSupport.isAnnotated(selector.getJavaClass(), LoadTest.class);
+            UniqueId testUniqueId = engineId.append("class", selector.getClassName());
+
+            TestDescriptor classDescriptor = new AbstractTestDescriptor(
+                    testUniqueId,
+                    selector.getJavaClass().getSimpleName(),
+                    ClassSource.from(selector.getJavaClass())) {
+                @Override
+                public Type getType() {
+                    return Type.CONTAINER;
+                }
+            };
+
+            engineDescriptor.addChild(classDescriptor);
+            addTestMethodDescriptors(selector.getJavaClass(), classDescriptor, testUniqueId,
+                    selectAllMethods, configuration);
+        });
+    }
+
+    /**
+     * Identify all the test cases to be executed
+     * @param testClass
+     * @param classDescriptor
+     * @param classUniqueId
+     * @param selectAllMethods
+     * @param configuration
+     */
+    private void addTestMethodDescriptors(Class<?> testClass, TestDescriptor classDescriptor, UniqueId classUniqueId,
+                                          boolean selectAllMethods, JupiterConfiguration configuration) {
+        Method[] methods = testClass.getDeclaredMethods();
+        for (Method method : methods) {
+            if (selectAllMethods || AnnotationSupport.isAnnotated(method, LoadTest.class)) {
+                UniqueId testUniqueId = classUniqueId.append("method", method.getName());
+
+                TestDescriptor child = new TestMethodTestDescriptor(testUniqueId, testClass, method, configuration);
+
+                classDescriptor.addChild(child);
+            }
+        }
     }
 
     @Override
@@ -67,7 +138,7 @@ public class LoadTestEngine implements TestEngine {
      */
     private void executeTestDescriptor(TestDescriptor descriptor, EngineExecutionListener listener) {
         if (descriptor.isTest()) {
-            if(loadTestExecutor.run(descriptor)) {
+            if (loadTestExecutor.run(descriptor)) {
                 listener.executionFinished(descriptor, TestExecutionResult.successful());
             } else {
                 listener.executionFinished(descriptor,
